3ff2c81eeee602dc38a9f077fde01385faf8050d
Onlygideon <stephengideon22@gmail.com>
2022-12-08 21:08:11 +0100

    add cors orign

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://codygo-hotel-ranking.netlify.app',
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
